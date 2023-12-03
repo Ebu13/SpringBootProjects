4829da6c3e21cd46e7581271657994ff46789c97
Ebubekir Nazlı <101016149+Ebu13@users.noreply.github.com>
2023-12-03 19:29:56 +0300

    new

---
+++ rentACar/rentACar/src/main/java/kodlama/io/rentACar/dataAccess/concretes/InMemoryBrandRepository.java
@@ -0,0 +1,31 @@
+package kodlama.io.rentACar.dataAccess.concretes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
+import kodlama.io.rentACar.entities.concretes.Brand;
+
+public class InMemoryBrandRepository implements BrandRepository{
+	
+	private List<Brand> brands;
+	
+		
+	public InMemoryBrandRepository() {
+		brands=new ArrayList<Brand>();
+		brands.add(new Brand(1,"Volvo"));
+		brands.add(new Brand(2,"BMW"));
+		brands.add(new Brand(3,"Mercedes"));
+		brands.add(new Brand(4,"Peugeot"));
+		brands.add(new Brand(5,"Audi"));
+		brands.add(new Brand(6,"TOGG"));
+	}
+
+
+	@Override
+	public List<Brand> getAll() {
+		
+		return brands;
+	}
+	
+}
